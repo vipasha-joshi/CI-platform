8d578a2abb177e2b9b06d6078bdf7e1fa1808e6e
Vipasha Joshi <vipasha.joshi2408@gmail.com>
2023-02-10 18:06:35 +0530

    login page in mvc

---
+++ MVC/CI_platform/CI_platform_web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CI_platform_web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
