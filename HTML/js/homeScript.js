6c487db0befab4eeabcada8c01fcd12569e0310c
Vipasha Joshi <vipasha.joshi2408@gmail.com>
2023-02-21 17:59:49 +0530

    rating and related mission page

--- HTML/js/homeScript.js
+++ HTML/js/homeScript.js
@@ -20,35 +20,61 @@ function showList(e) {
     var $gridCont = $('.grid-container');
     e.preventDefault();
     $gridCont.hasClass('list-view') ? $gridCont.removeClass('list-view') : $gridCont.addClass('list-view');
-  }
-  function gridList(e) {
+}
+function gridList(e) {
     var $gridCont = $('.grid-container')
     e.preventDefault();
     $gridCont.removeClass('list-view');
-  }
-  
-  $(document).on('click', '.btn-grid', gridList);
-  $(document).on('click', '.btn-list', showList);
+}
 
-  const checkboxes = document.querySelectorAll(".checkbox");
-  
-  const filterList = document.getElementById("selected-filters");
-  
-  var listArray = [];
-  
-  for(var checkbox of checkboxes){
-      checkbox.addEventListener('click', function(){
-          if(this.checked == true){
-              listArray.push(this.value);
-              filterList.innerHTML = listArray.join(' / ');
-              filterList.createElement('p');
-  
-              console.log(this.value);
-          }
-          else{
-              console.log('you unchecked the checkbox');
-              listArray = listArray.filter(e => e !== this.value);
-              filterList.innerHTML = listArray.join(' / ');
-          }
-      })
-  }
+$(document).on('click', '.btn-grid', gridList);
+$(document).on('click', '.btn-list', showList);
+
+const checkboxes = document.querySelectorAll(".checkbox");
+
+const filterList = document.getElementById("selected-filters");
+
+var listArray = [];
+
+for (var checkbox of checkboxes) {
+    checkbox.addEventListener('click', function () {
+        if (this.checked == true) {
+            listArray.push(this.value);
+            filterList.innerHTML = listArray.join(' / ');
+            filterList.createElement('p');
+
+            console.log(this.value);
+        }
+        else {
+            console.log('you unchecked the checkbox');
+            listArray = listArray.filter(e => e !== this.value);
+            filterList.innerHTML = listArray.join(' / ');
+        }
+    })
+}
+$(function () {
+
+    $(document).on({
+        mouseover: function (event) {
+            $(this).find('.far').addClass('star-over');
+            $(this).prevAll().find('.far').addClass('star-over');
+        },
+        mouseleave: function (event) {
+            $(this).find('.far').removeClass('star-over');
+            $(this).prevAll().find('.far').removeClass('star-over');
+        }
+    }, '.rate');
+
+
+    $(document).on('click', '.rate', function () {
+        if (!$(this).find('.star').hasClass('rate-active')) {
+            $(this).siblings().find('.star').addClass('far').removeClass('fas rate-active');
+            $(this).find('.star').addClass('rate-active fas').removeClass('far star-over');
+            $(this).prevAll().find('.star').addClass('fas').removeClass('far star-over');
+        } else {
+            console.log('has');
+        }
+    });
+
+});
+
